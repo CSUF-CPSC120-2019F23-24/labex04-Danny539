de024d5bca21058628d6517a2186317c861f95cf
danny539 <danny539@csu.fullerton.edu>
2019-09-14 14:59:35 -0700

    prob03-danny539

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,20 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+
+{
+
+  const double sales_rate = 0.18;
+  const int my_sales = 2103419227;
+  long nextyear;
+
+  nextyear = (sales_rate + my_sales) + my_sales;
+
+  std::cout << "last years sales were " << my_sales << std::endl;
+  std::cout << "this years sales prediction will be " << nextyear << std::endl;
+
+  return 0;
+
+}
